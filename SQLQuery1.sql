eb839a3574f05c0dcb4f0b89a50ccaf25dcd31b7
ST10037089-Alisa Diya Thool <104021252+alisadthool@users.noreply.github.com>
2025-03-19 18:23:07 +0200

    code commits

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [Id]
+      ,[SecondName]
+      ,[Score]
+      ,[FirstName]
+  FROM [aspnet-NinetyOne-bea8b432-7353-4dcd-b214-f0e4207f52dc].[dbo].[People]
+
